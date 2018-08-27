ad664195239cca293dff17ae5712c1d38718405b
Alessandro Ercolani <ale.ercolani@gmail.com>
2018-08-27 15:43:55 +0200

    minor

--- app/apiModels/Vocabulary.java
+++ app/apiModels/Vocabulary.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.*;
 /**
  * A vocabulary for standardization
  */
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaPlayFrameworkCodegen", date = "2018-07-25T16:23:23.837+02:00")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaPlayFrameworkCodegen", date = "2018-08-18T11:53:14.465+02:00")
 
 @SuppressWarnings({"UnusedReturnValue", "WeakerAccess"})
 public class Vocabulary   {
--- app/controllers/VocabularyApiController.java
+++ app/controllers/VocabularyApiController.java
@@ -20,7 +20,7 @@ import play.Configuration;
 
 import swagger.SwaggerUtils.ApiAction;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaPlayFrameworkCodegen", date = "2018-07-25T16:23:23.837+02:00")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaPlayFrameworkCodegen", date = "2018-08-18T11:53:14.465+02:00")
 
 public class VocabularyApiController extends Controller {
 
